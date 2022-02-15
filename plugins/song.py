e136ee92a72359664e5668dbea5bec3f06d1968c
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 04:38:48 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -75,7 +75,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"♬ <b>Title : {title}</b>\n♬ <b>Duration : {duration}</b>\n♬ <b> Requested By : {message.from_user.mention}"
+        rep = f"♬ <b>Title : {title}</b>\n♬ <b>Duration : {duration}</b>\n♬ <b> Requested By : {message.from_user.mention}</b>"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
