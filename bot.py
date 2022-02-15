cbdd05c8b83e3b22955a7c00df36a9603f369826
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 03:34:12 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+from pyrogram import Client
+
+Client = Client(
+  "Music Bot",
+  api_hash=os.environ.get("API_ID"),
+  api_id=int(os.environ.get("API_ID")),
+  bot_token=os.environ.get("BOT_TOKEN"),
+  plugins=dict(root="plugins")
+)
+
+Client.start()
+print("Bot has started")
+
+Client.stop()
+print("Bot has stopped")
+
+Client.run()
