fe98a8e866dd27633fa6401ceac6bf931767a05c
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 04:14:24 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -56,7 +56,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[Ria Music Bot]" 
+            performer = f"[Riya Music Bot]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
