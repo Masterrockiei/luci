eb8d630a6464c3279d8a0c0232464f79cb620f66
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-17 03:44:23 +0530

    Update pluginhelpers.py

--- plugins/function/pluginhelpers.py
+++ plugins/function/pluginhelpers.py
@@ -10,12 +10,12 @@ from typing import Callable, Coroutine, Dict, List, Tuple, Union
 
 import aiohttp
 from PIL import Image
-from pyrogram import Client
+from pyrogram import Client as pbot
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
 from config import Config
-from bot import Client as pbot
+from bot import pbot
 
 SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", " ")
 
