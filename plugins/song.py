481892c8a8528ade13da95a1e59fc4c96b005649
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 19:05:25 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -95,7 +95,7 @@ def a(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name, reply_to_message_id=message.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("☘️ Send Personally", callback_data="forward")]]))
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name, reply_to_message_id=message.message_id)
         m.delete()
     except Exception as e:
         m.edit('There is an error while processing your request.')
