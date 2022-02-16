8ac11d92f95723db1825b28f8e1a45bd05e9fb7c
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 14:55:16 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -67,9 +67,9 @@ def a(client, message):
             views = results[0]["views"]
 
             ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
-            # if time_to_seconds(duration) >= 1800:  # duration limit
-            #     m.edit("Exceeded 30mins cap")
-            #     return
+            if time_to_seconds(duration) >= 1800:  # duration limit
+                 m.edit("Exceeded 30mins cap")
+                 return
 
             performer = f"[Riya Music Bot]" 
             thumb_name = f'thumb{message.message_id}.jpg'
