9cbc2a2882052909cc1125a5119bee0554dc4e21
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 10:36:21 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -33,7 +33,7 @@ def a(client, message):
         query += ' ' + str(i)
     print(query)
     m = message.reply("⬆️ Processing")
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    ydl_opts = {"format": "bestaudio[ext=mp3]"}
     try:
         results = []
         count = 0
