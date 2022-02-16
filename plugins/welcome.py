3c0b8cedde46c75812e8301b4adf2a7949ed655b
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 15:50:45 +0530

    Create welcome.py

---
+++ plugins/welcome.py
@@ -0,0 +1,18 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+
+
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid= message.chat.id
+	m=await bot.send_message(text=f"Welcome {message.from_user.mention} to Kerala Music Hub ,  Happy to see you here",chat_id=chatid)
+	await asyncio.sleep(6)
+        await m.delete()
+@Client.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	n=await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	await asyncio.sleep(6)
+        await n.delete()
