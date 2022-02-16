61d0697043c7f10b49044e3dbe8d8df1aad7404b
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-16 14:10:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config(object):
-  GROUP_ID = int(os.environ.get("GROUP_ID", "-1001565165506")
+  GROUP_ID = int(os.environ.get("GROUP_ID", "-1001565165506"))
   ADMINS = [id for id in os.environ.get("ADMINS").split("1921448473")]
