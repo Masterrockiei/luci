cb625078ddab728bbcea0a7d62c594f692d060d7
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-21 11:58:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
   GROUP_ID = int(os.environ.get("GROUP_ID", "-1001565165506"))
   ADMINS = [id for id in os.environ.get("ADMINS").split(" ")]
   SESSION_NAME = os.environ.get("RiyaMusicBot", "session")
-  API_HASH = os.environ.get("API_HASH")
-  API_ID =  int(os.environ.get("API_ID", ""))
+  API_HASH = os.environ.get("api_hash")
+  API_ID =  int(os.environ.get("api_id"))
   BOT_TOKEN = os.environ.get("BOT_TOKEN")
   
