b052fb8744f1ec8be2c92de1b21bb6ae06cb4593
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-21 12:15:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 GROUP_ID = int(getenv("GROUP_ID", "-1001565165506"))
 ADMINS = [id for id in getenv("ADMINS").split(" ")]
 SESSION_NAME = getenv("RiyaMusicBot", "session")
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
   
