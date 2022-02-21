d0e49c7875630f95988123a9694a94edad678576
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-02-21 11:52:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
   GROUP_ID = int(os.environ.get("GROUP_ID", "-1001565165506"))
   ADMINS = [id for id in os.environ.get("ADMINS").split(" ")]
-  SESSION_NAME = "RiyaMusicBot"
+  SESSION_NAME = os.environ.get("RiyaMusicBot", "session")
   API_HASH = os.environ.get("API_HASH")
   API_ID =  int(os.environ.get("API_ID"))
   BOT_TOKEN = os.environ.get("BOT_TOKEN")
