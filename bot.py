474c5eb83cb9ed399f2341457bd32b0a1c8c31d3
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-04-24 16:19:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client
 from config import API_HASH, API_ID, BOT_TOKEN
 pbot = Client(
-  "RiyaMusicBot",
+  "SophieMusicBot",
   API_ID,
   API_HASH,
   bot_token=BOT_TOKEN,
